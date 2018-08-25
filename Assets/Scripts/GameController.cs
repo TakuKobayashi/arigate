d3edfa51b954304d41b9b4a2b3012c72659f29bd
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-08-25 20:37:36 +0900

    ゲームの基本的な処理を作ってみた

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class GameController : SingletonBehaviour<GameController> {
+
+    public float ElapsedSecond { private set; get; }
+
+    private List<TargetSymbol> appearSymbols = new List<TargetSymbol>();
+    [SerializeField] private float baseGivePoint = 100;
+    public float CurrentPoint{ private set; get; }
+    public Action OnHit = null;
+
+    public TargetSymbol AppearSymbol(Vector3 appearPoint, GameObject appearedObject){
+        TargetSymbol targetSymbol;
+#if UNITY_ANDROID || UNITY_EDITOR
+        GoogleARCore.Anchor anchor = GoogleARCore.Session.CreateAnchor(Pose.identity);
+        anchor.transform.parent = this.transform;
+        targetSymbol = Util.InstantiateTo<TargetSymbol>(anchor.gameObject, appearedObject);;
+#else
+        targetSymbol = Util.InstantiateTo<TargetSymbol>(this.gameObject, appearedObject);
+#endif
+        appearSymbols.Add(targetSymbol);
+        return targetSymbol;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        ElapsedSecond += Time.deltaTime;
+        CheckHitAndGetPoint();
+	}
+
+    public void CheckHitAndGetPoint(){
+        TargetSymbol hitsym = appearSymbols.Find(sym => sym.IsHit(Camera.main.transform.position));
+        if(hitsym != null){
+            CurrentPoint += Mathf.Max(baseGivePoint - ElapsedSecond, 0);
+            appearSymbols.Remove(hitsym);
+            ClearTime();
+            if(OnHit != null){
+                OnHit();
+            }
+        }
+    }
+
+    public void ClearTime(){
+        ElapsedSecond = 0f;
+    }
+}
---
+++ Assets/Scripts/TargetSymbol.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetSymbol : MonoBehaviour
+{
+    [SerializeField] private float hitDistance;
+
+    public bool IsHit(Vector3 targetPosition)
+    {
+        float distance = Vector3.SqrMagnitude(transform.position - targetPosition);
+        Debug.Log(distance);
+        return distance < hitDistance;
+    }
+}
