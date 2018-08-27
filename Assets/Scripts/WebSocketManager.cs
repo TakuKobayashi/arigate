4514a3dc04f2527b9edaa33ac1e1715691729e6c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-08-27 21:39:20 +0900

    Websocket4Netを使った方のつなぎ方を実践

--- Assets/Scripts/WebSocketManager.cs
+++ Assets/Scripts/WebSocketManager.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
-using WebSocketSharp;
+using WebSocket4Net;
 using System;
+using System.Security.Authentication;
+using System.Collections;
 using System.Collections.Generic;
 
 public class WebSocketManager : SingletonBehaviour<WebSocketManager>{
 	private WebSocket ws = null;
-	private List<MessageEventArgs> receiveMessageQueue = new List<MessageEventArgs>();
+    private bool isSocketOpened = false;
+	private List<string> receiveMessageQueue = new List<string>();
+    private List<byte[]> receiveDataQueue = new List<byte[]>();
 	public Action<string> OnReceiveMessage = null;
-    
-	void Update()
+    public Action<byte[]> OnReceiveData = null;
+
+    void Update()
 	{
 		if (receiveMessageQueue.Count > 0)
 		{
@@ -16,10 +21,18 @@ public class WebSocketManager : SingletonBehaviour<WebSocketManager>{
 			{
 				if (OnReceiveMessage != null)
 				{
-					OnReceiveMessage(receiveMessageQueue[i].Data);
+					OnReceiveMessage(receiveMessageQueue[i]);
 				}
 			}
+            for (int i = 0; i < receiveDataQueue.Count; ++i)
+            {
+                if (OnReceiveData != null)
+                {
+                    OnReceiveData(receiveDataQueue[i]);
+                }
+            }
 			receiveMessageQueue.Clear();
+            receiveDataQueue.Clear();
 		}
 	}
 
@@ -27,35 +40,43 @@ public class WebSocketManager : SingletonBehaviour<WebSocketManager>{
 	{
 		Debug.Log(wsUrl);
 		// WebSocketのechoサーバ.
-		this.ws = new WebSocket(wsUrl);
+        this.ws = new WebSocket(wsUrl, sslProtocols: SslProtocols.Tls12 | SslProtocols.Tls11 | SslProtocols.Tls);
 
 		// WebSocketをOpen.
-		this.ws.OnOpen += (sender, e) =>
+        this.ws.Opened += (sender, e) =>
 		{
+            isSocketOpened = true;
 			Debug.Log("[WS] Open");
 		};
 
 		// メッセージを受信.
-		this.ws.OnMessage += (sender, e) =>
+        this.ws.MessageReceived += (sender, e) =>
 		{
-			receiveMessageQueue.Add(e);
-			Debug.Log("[WS]Receive message: " + e.Data);
+            receiveMessageQueue.Add(e.Message);
+            Debug.Log("[WS]Receive message: " + e.Message);
 		};
+
+        // メッセージを受信.
+        this.ws.DataReceived += (sender, e) =>
+        {
+            receiveDataQueue.Add(e.Data);
+            Debug.Log("[WS]Receive data: " + e.Data);
+        };
         
 		// WebSoketにErrorが発生.
-		this.ws.OnError += (sender, e) =>
+        this.ws.Error += (sender, e) =>
 		{
-			Debug.Log("[WS]Error: " + e.Message);
+            Debug.Log("[WS]Error: " + e.Exception.Message);
 		};
 
 		// WebSocketがClose.
-		this.ws.OnClose += (sender, e) =>
+        this.ws.Closed += (sender, e) =>
 		{
 			Debug.Log("[WS]Close");
 		};
         
 		// WebSocketに接続.
-		this.ws.Connect();
+        this.ws.Open();
 	}
 
 	public void Send(string message){
